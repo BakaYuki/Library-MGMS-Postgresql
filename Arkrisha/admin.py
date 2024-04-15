4bf596c5e55906a9c9c9e1b502769aa59a3c05bd
Arku-Stha <arkrisha02@gmail.com>
2024-04-15 17:28:16 +0545

    as

--- Arkrisha/admin.py
+++ Arkrisha/admin.py
@@ -21,7 +21,7 @@ def admin_panel():
     
     if st.session_state.get('admin_logged_in', True):
         st.title("Admin Panel")
-        admin_option = st.sidebar.selectbox("Choose Option", ["Add Book", "Update Book", "Add Student", "Log Out"])
+        admin_option = st.sidebar.selectbox("Choose Option", ["Add Book", "Update Book", "Add Student", "View Book", "Log Out"])
         
         if admin_option == "Add Book": # Adding book functionality
             add_book()
@@ -33,6 +33,9 @@ def admin_panel():
             
         elif admin_option == "Add Student":
             add_student()  # add student functionality
+            
+        elif admin_option == "View Book":
+            view_book()
 
 
         elif admin_option == "Log Out":
