747eb03efe7a1b79ecd6347689e3a91c63bd54d9
Arku-Stha <arkrisha02@gmail.com>
2024-04-15 16:42:51 +0545

    as

--- Arkrisha/student.py
+++ Arkrisha/student.py
@@ -3,9 +3,8 @@ from issuebook import *
 from returnbook import *
 def student_panel():
     student_option = st.sidebar.selectbox("Choose Option", ["Issue Book", "Return Book"])
+    
     if student_option == "Issue Book":
-        # if 'userr' not in st.session_state:
-        #     st.write("Please login first")
         issue_book()
         
     elif student_option == "Return Book":
